46a0da8a438bc95527c160ce5e01ebf686decef3
Maxime Leroy <maxime.leroy.4@ulaval.ca>
2018-11-30 16:49:32 -0500

    add(jeu tp3)

--- TP3_SBC/sbc.pl
+++ TP3_SBC/sbc.pl
@@ -10,19 +10,21 @@
 
 % données du problème : fait( X ) - à  ajouter
 
-
 % Règles de la base de connaissances : si ... alors ... - à ajouter
-si blizzard et rts et futuriste alors stacraft.
-si blizzard et rts et fantasy alors warcraft.
-si blizzard et moba alors hots.
-si blizzard et mmorpg alors wow.
-si blizzard et rpg alors diablo.
-si blizzard et fps alors overwatch.
-si ea et fps alors battlefield.
-si ea et mmorpg et futuriste alors swtor.
-si ea et rpg et fantasy alors dragon.
-si riot et moba alors lol.
-si blizzard et fantasy et pc et online ou rpg et online alors wow.
+si distance alors tireur.
+si magie ou sort alors mage.
+si resistance ou melee alors tank.
+si tireur et glace ou reine alors ashe.
+si tireur et feu ou reine alors caitlyn.
+si tireur et eau ou rat alors twitch.
+si tireur et pistolet ou pirate alors mf.
+si mage et feu ou elementaire alors brand.
+si mage et glace ou pheonix alors anivia.
+si mage et eau ou rat alors fizz.
+si mage et pistolet ou artiste alors jhin.
+si tank et rocher ou elementaire alors malphite.
+si tank et glace ou artiste alors mundo.
+si tank et pistolet ou pirate alors gankplang.
 
 % ch_arriere/1 : moteur d inference fonctionnant en chainage arriere
 ch_arriere( But ) :-
@@ -57,7 +59,7 @@ recherche_fait( Cond1 et Cond2 ) :-
 recherche_fait( Cond1 ou Cond2 ) :-
    recherche_fait( Cond1 ) ; recherche_fait( Cond2 ).
 
-% main_game/0 : point d'entrée secondaire (garde la liste des faits)
+% main_game/0 : boucle de jeu principale
 main_game :-
    writeln('Ajoutez un fait:'),
    read(E),
@@ -70,8 +72,41 @@ main_game :-
    writeln('Fait déjà existant !'),
    main_game.
 
+
+
+% clear/O : vide la liste des faits
+clear :-
+      retractall(fait(_)).
+
+% jouer_avec_fait/0 point d'entrée secondaire (garde la liste des faits)
+jouer_avec_fait:-
+   ch_avant.
+
 % play/0 : point d'entrée (vide la liste des faits)
 play :-
    retractall(fait(_)),
    main_game.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
